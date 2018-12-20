9ff7de4e6e06514a814909a52de77441835f892c
vatinidian <mvenkatesh238@gmail.com>
2018-12-20 14:50:32 +0530

    Update ProgramSolutions.js

--- ProgramSolutions.js
+++ ProgramSolutions.js
@@ -61,3 +61,90 @@ function pigIt(str){
   
   return aStrings.join(" ");
 }
+
+// 5. https://www.codewars.com/kata/whats-a-perfect-power-anyway/
+function isPP(n){
+ if(n<4  || isPrime(n)) {
+   return null;
+ }
+ let iMyBase = 2;
+ let iMyPower = 0;
+ let iTempNum = n;
+ while(iTempNum !== 1){
+   if(iTempNum%iMyBase === 0) {
+     iTempNum = iTempNum/iMyBase;
+     iMyPower += 1;
+   } else {
+     iTempNum = n;
+     iMyBase += 1;
+     iMyPower = 0;
+   }
+ }
+ return iMyPower > 1 ? [iMyBase, iMyPower] : null;
+}
+
+function isPrime(num) {
+  let iCondition = Math.sqrt(num)+1 ;
+  for(let i =2; i < iCondition; i++) {
+    if(num%i === 0){
+      return false;
+    }
+  } 
+  return num !== 1;
+}
+
+// 6. https://www.codewars.com/kata/primes-in-numbers/
+function primeFactors(n){
+    let iPrime = 2;
+    let sOutput = "";
+    let oMap = {};
+    
+    while(n > 1){
+      if(n%iPrime === 0){
+        n = n / iPrime;
+        if(oMap[iPrime]) {
+          oMap[iPrime] += 1;
+        } else {
+          oMap[iPrime] = 1;
+        }
+      } else {
+        iPrime = findNextPrime(iPrime);
+      }
+    }
+    
+    for(let sKey in oMap){
+      if(oMap[sKey] > 1) {
+        sOutput += "(" + sKey + "**"+ oMap[sKey] + ")";
+      } else {
+        sOutput += "("+sKey+")"
+      }
+    };
+    return sOutput;
+}
+
+function findNextPrime(num) {
+  num = num + 1;
+  while(!isPrime(num)) {
+    num++;
+  }
+  
+  return num;
+}
+
+// 7. https://www.codewars.com/kata/moving-zeros-to-the-end/
+function moveZeros(arr) {
+  let iLength = arr.length;
+  let iCount = 0;
+  for(let i =iLength-1; i >= 0; i--) {
+    if(arr[i] === 0) {
+      arr.splice(i,1);
+      iCount++;
+    }
+  }
+  
+  for(let i =0; i < iCount; i++) {
+    arr.push(0);
+  }
+  
+  return arr;
+}
